d83c803f588ee2c34ef8d631a31882ecd35fc23a
GustavoPellanda <gusfipe@gmail.com>
2026-03-15 22:28:12 -0300

    Starting project.

---
+++ OTA_transactions/include/conifg.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Connnection to Wi-Fi network:
+#define WIFI_SSID     "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+
+// Connection to the server providing the version and firmware files:
+#define VERSION_URL   "http://192.168.1.100:8080/version.json"
+#define FIRMWARE_URL  "http://192.168.1.100:8080/firmware.bin"
+
+// Application version and OTA update interval:
+#define APP_VERSION   "1.0.0"
+#define OTA_INTERVAL  15000   // ms
